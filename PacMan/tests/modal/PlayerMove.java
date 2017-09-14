03b6e4db4806908e3b99bced39ce4240e127a758
markuspokkinen <markuspokkinen@gmail.com>
2017-09-14 15:37:47 +0300

    Junit test

---
+++ PacMan/tests/modal/PlayerMove.java
@@ -0,0 +1,25 @@
+package modal;
+
+import java.awt.Point;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import modal.Map;
+import modal.MovementLogic;
+
+public class PlayerMove {
+	private static MovementLogic ml;
+	private static Map map;
+	
+	@BeforeClass
+	public static void start() {
+		System.out.println("before test");
+		
+	}
+	@Test
+	public void moveUp() {
+		
+	}
+
+}
