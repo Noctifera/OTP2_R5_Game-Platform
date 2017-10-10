a560a83f1b0a7f26dc55397d463273437c06b0d1
palo <palo@DESKTOP-H9MNTQ1.wlan.metropolia.fi>
2017-10-10 13:26:31 +0300

    Highscore JDoc tehty

--- PacMan/src/modal/HighScore.java
+++ PacMan/src/modal/HighScore.java
@@ -5,7 +5,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author kari-antti
+ *
+ */
 public class HighScore {
 
 	/*
@@ -20,18 +24,39 @@ public class HighScore {
 	private ArrayList<String> score = new ArrayList<>();
 	private ArrayList<String> name = new ArrayList<>();
 	private ArrayList<String> date = new ArrayList<>();
+	
+	/**
+	 * Getter for score arraylist that holds the highscores score data
+	 * @return returns the score arraylist
+	 */
 
 	public ArrayList<String> getScore() {
 		return score;
 	}
+	
+	/**
+	 * Getter for name arraylist that holds the highscores player name data
+	 * @return returns the name arraylist
+	 */
 
 	public ArrayList<String> getName() {
 		return name;
 	}
 
+	/**
+	 * Getter for date arraylist that holds the highscores date data
+	 * @return returns the date arraylist
+	 */
+	
 	public ArrayList<String> getDate() {
 		return date;
 	}
+	
+	/**
+	 * Creates database connection by using jdbc driver
+	 * @return returns the connection {@link Connection}
+	 * @throws Exception catches the Exception e and posts it in console
+	 */
 
 	public Connection getConnection() throws Exception {
 
@@ -57,6 +82,13 @@ public class HighScore {
 
 	}
 
+	/**
+	 *  This method is used to post player data to the highscore database
+	 * @param score int value for players score
+	 * @param playername String for players name
+	 * @param databasename String for databases name
+	 * @return returns true if post to database is successful
+	 */
 	public boolean post(int score, String playername, String databasename) {
 		boolean sucess = false;
 		Connection conn = null;
@@ -79,6 +111,12 @@ public class HighScore {
 		}
 		return sucess;
 	}
+	
+	/**
+	 * This method selects the data from the database and sorts it by score
+	 * highscore data is then added to score-, name- and date arraylists
+	 * @param databasename selects the database to choose data from
+	 */
 
 	public void selectFromDatabase(String databasename) {
 		Connection conn = null;
@@ -113,6 +151,11 @@ public class HighScore {
 		}
 	}
 
+	/**
+	 * This method creates String of todays date
+	 * @return returns todays date in year / month / date format
+	 */
+	
 	public String currentDate() {
 		java.util.Date date = new java.util.Date();
 		java.text.SimpleDateFormat time = new java.text.SimpleDateFormat("yyyy-MM-dd");
@@ -120,6 +163,10 @@ public class HighScore {
 		return currentDate;
 	}
 
+	/**
+	 * This method is only used in JUnit tests
+	 * Method clearTable completely clears the database table
+	 */
 	public void clearTable() {
 		Connection conn = null;
 		try {
