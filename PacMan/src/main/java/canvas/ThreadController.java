f7d0fdfbde0965c817e9ca77f2c8451562798295
markuspokkinen <markuspokkinen@gmail.com>
2017-11-29 12:34:41 +0200

    Sprintti 23 vericalbox horizontalbox

--- PacMan/src/main/java/canvas/ThreadController.java
+++ PacMan/src/main/java/canvas/ThreadController.java
@@ -9,26 +9,44 @@ import characters.Player;
 public class ThreadController {
 
 	private ArrayList<Character_Thread> threads;
+	private Player player;
+	private Ghost[] ghlist;
+	private boolean active = false;
 
-	private void inis(Player player, Ghost[] ghlist) {
+	public ThreadController(Player player, Ghost[] ghlist) {
+		this.player = player;
+		this.ghlist = ghlist;
+	}
+
+	private void inis() {
+
 		threads = new ArrayList<>();
+
 		for (int i = 0; i < ghlist.length; i++) {
 			threads.add(new Character_Thread(ghlist[i]));
 		}
+
 		threads.add(new Character_Thread(player));
 	}
 
-	public void startThreads(Player player, Ghost[] ghlist) {
-		inis(player, ghlist);
+	public void startThreads() {
+		inis();
 		for (Character_Thread ct : threads) {
 			ct.start();
 		}
-
+		active = true;
 	}
 
 	public void suppress() {
 		for (Character_Thread ct : threads) {
 			ct.suppress();
 		}
+		active = false;
 	}
+
+	public boolean isActive() {
+		return active;
+	}
+	
+
 }
