e3126510d3a0242718fe246036fe22dad0ab7e79
markuspokkinen <markuspokkinen@gmail.com>
2017-12-04 13:36:51 +0200

    yhteys korjaus 3

--- PacMan/src/main/java/hibernate_PacMan/DataBaseConnection.java
+++ PacMan/src/main/java/hibernate_PacMan/DataBaseConnection.java
@@ -15,6 +15,7 @@ import org.hibernate.exception.JDBCConnectionException;
 
 /**
  * Used to save and get data from our mysql database
+ * 
  * @author kari-antti
  * @version 1.0
  **/
@@ -23,52 +24,55 @@ public class DataBaseConnection {
 	private static List<HighScores> scores = null;
 	private static List<MapsTable> mapList = null;
 	private static MapsTable usedMap = null;
+
 	/**
 	 * This method creates String of todays date
+	 * 
 	 * @return returns todays date in year / month / date format
 	 */
-	
+
 	private static String currentDate() {
 		java.util.Date date = new java.util.Date();
 		java.text.SimpleDateFormat time = new java.text.SimpleDateFormat("yyyy-MM-dd");
 		String currentDate = time.format(date);
 		return currentDate;
 	}
+
 	public static HashMap<Point, String> setFirstMap() {
 		return readOneMap(mapList.get(0).getMapName());
 	}
-	
+
 	private static Session OpenConnectionToDataBase() {
 		SessionFactory sessFac = null;
 
-		 StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate_localhost.cfg.xml").build();
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate_localhost.cfg.xml").build();
 		System.out.println("Configuration tiedosto ladattu");
-		
+
 		try {
 			sessFac = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-			
-		} catch (JDBCConnectionException je) {
+
+		} catch (Exception e) {
 			System.out.println("istonto virhe");
 			registry = new StandardServiceRegistryBuilder().configure("hibernate_jenkins.cfg.xml").build();
 			try {
-			sessFac = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-			}catch (Exception e1) {
-				// TODO: handle exception
-				
+				sessFac = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+			} catch (Exception e1) {
+				StandardServiceRegistryBuilder.destroy(registry);
+				e1.printStackTrace();
 			}
-		}catch (Exception e) {
-			StandardServiceRegistryBuilder.destroy(registry);
-			e.printStackTrace();
+
 		}
-			
+
 		Session sess = sessFac.openSession();
 		return sess;
 	}
-	
+
 	/**
 	 * This method selects the data from the database and sorts it by score
 	 * highscore data is then added to score-, name- and date arraylists
-	 * @param databasename selects the database to choose data from
+	 * 
+	 * @param databasename
+	 *            selects the database to choose data from
 	 */
 
 	@SuppressWarnings("unchecked")
@@ -76,28 +80,30 @@ public class DataBaseConnection {
 		Session conn = null;
 		try {
 			conn = OpenConnectionToDataBase();
-		
+
 			scores = conn.createQuery("from HighScores order by Score desc").list();
-			for(HighScores h: scores) {
+			for (HighScores h : scores) {
 				System.out.println(h);
 			}
 
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
-		}finally {
+		} finally {
 			conn.close();
 		}
 	}
-	public static List<HighScores> scoreForMap(String mString){
+
+	public static List<HighScores> scoreForMap(String mString) {
 		List<HighScores> list = new ArrayList<>();
-		for(HighScores hs: scores) {
-			if(hs.getMapName().getMapName().equals(mString)) {
+		for (HighScores hs : scores) {
+			if (hs.getMapName().getMapName().equals(mString)) {
 				list.add(hs);
 			}
 		}
-		
+
 		return list;
 	}
+
 	public static boolean post(int score, String playername) {
 		boolean sucess = false;
 		Session conn = null;
@@ -105,18 +111,18 @@ public class DataBaseConnection {
 			conn = OpenConnectionToDataBase();
 			Transaction tran = conn.beginTransaction();
 
-			HighScores hs = new HighScores(currentDate(),score,playername, usedMap);
+			HighScores hs = new HighScores(currentDate(), score, playername, usedMap);
 			conn.saveOrUpdate(hs);
 			tran.commit();
 			sucess = true;
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
-		}finally {
+		} finally {
 			conn.close();
 		}
 		return sucess;
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "unused" })
 	public static void getAllMapsFromDataBase() {
 		Session sess = OpenConnectionToDataBase();
@@ -137,7 +143,7 @@ public class DataBaseConnection {
 			sess.close();
 		}
 	}
-	
+
 	public static String[] allMapNames() {
 		String[] names = new String[mapList.size()];
 		for (int i = 0; i < mapList.size(); i++) {
@@ -145,7 +151,7 @@ public class DataBaseConnection {
 		}
 		return names;
 	}
-	
+
 	public static HashMap<Point, String> readOneMap(String mapName) {
 		for (MapsTable m : mapList) {
 			if (m.getMapName().equals(mapName)) {
@@ -155,9 +161,10 @@ public class DataBaseConnection {
 		}
 		return null;
 	}
-	public static List<String> getMapNames(){
+
+	public static List<String> getMapNames() {
 		ArrayList<String> list = new ArrayList<>();
-		for(MapsTable mt: mapList) {
+		for (MapsTable mt : mapList) {
 			list.add(mt.getMapName());
 		}
 		return list;
@@ -170,6 +177,7 @@ public class DataBaseConnection {
 	public static List<MapsTable> getMapList() {
 		return mapList;
 	}
+
 	public static MapsTable getUsedMap() {
 		return usedMap;
 	}
